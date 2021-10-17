a00748d7ffd57a311341e121854e35c246611f79
jfcaballerop <jfcaballerop@gmail.com>
2021-10-17 19:38:47 +0200

    forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { LoginControl, Page } from './components/Body';
+import Forms from './components/Forms';
 import Numbers from './components/Numbers.jsx';
+import Reservation from './components/Reservation';
 import * as Title from './components/Title';
 
 function App() {
@@ -16,6 +18,11 @@ function App() {
       <hr />
 
       <Numbers />
+
+      <hr></hr>
+      <Forms />
+      <hr></hr>
+      <Reservation />
 
     </div>
   );
